d7301aff322931dc6f3a7703f80d62b83822e8d6
DanSch <daniel.schaberreiter@gmail.com>
2022-10-30 23:47:28 +0100

    create/implement hero section

---
+++ src/app/hero-section/hero-section.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+const Typewriter = require('t-writer.js');
+
+@Component({
+  selector: 'app-hero-section',
+  templateUrl: './hero-section.component.html',
+  styleUrls: ['./hero-section.component.scss']
+})
+export class HeroSectionComponent implements OnInit {
+  @ViewChild('tw', {static: true}) typewriterElement?: ElementRef;
+
+  faCaretDown = faCaretDown;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const writer = new Typewriter(this.typewriterElement?.nativeElement, {
+      loop: true,
+      typeColor: '#525255'
+    });
+
+  writer
+    .type("I build things for the web.")
+    .rest(750)
+    .clear()
+    .type("I come up with cool ideas.")
+    .rest(750)
+    .clear()
+    .type("I'm eager to learn new things.")
+    .rest(750)
+    .clear()
+    .start()
+  }
+}
