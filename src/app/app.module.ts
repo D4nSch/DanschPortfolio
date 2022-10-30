8f4267a7590f2d1830785e5e7fe8d36a41da45a1
DanSch <daniel.schaberreiter@gmail.com>
2022-10-30 23:42:16 +0100

    include t-write, fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutSectionComponent } from './about-section/about-section.component';
+import { WorkSectionComponent } from './work-section/work-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeroSectionComponent,
+    AboutSectionComponent,
+    WorkSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
