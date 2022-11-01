ab055d07640cb33d1f1a2d357f9bcd8164caa0ec
DanSch <daniel.schaberreiter@gmail.com>
2022-11-01 15:01:55 +0100

    add caret to work section

--- src/app/work-section/work-section.component.ts
+++ src/app/work-section/work-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-work-section',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkSectionComponent implements OnInit {
 
+  faCaretRight = faCaretRight;
   workToShow = "self";
 
   constructor() { }
