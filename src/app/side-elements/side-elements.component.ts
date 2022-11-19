29b1ccee11d783e46d9cf547b752f01ee96f6000
DanSch <daniel.schaberreiter@gmail.com>
2022-11-19 02:07:08 +0100

    add side-elements; email/social links

---
+++ src/app/side-elements/side-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-elements',
+  templateUrl: './side-elements.component.html',
+  styleUrls: ['./side-elements.component.scss']
+})
+export class SideElementsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
